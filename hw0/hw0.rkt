b0f6b248b9f67c48daa35a5a8c287de1b9a6d021
Alyssa Shteyn <alyssashteyn@cs.uchicago.edu>
2019-10-09 15:26:55 -0500

    adding hw0

--- hw0/hw0.rkt
+++ hw0/hw0.rkt
@@ -1,3 +1,25 @@
 #lang typed/racket
 (require "../include/cs151-core.rkt")
 (require "../include/cs151-image.rkt")
+;;drawing three pictures through Racket
+
+(define mushroom
+  (above
+   (ellipse 60 30 "solid" "red")
+   (rectangle 20 40 "solid" "brown")))
+
+(define dot(circle 5 "solid" "white"))
+
+(overlay/align "center" "top" dot mushroom)
+
+;;(define lizard
+
+(define pig
+(below
+  (circle 40 "solid" "red")
+  (ellipse 30 15 "solid" "pink")))
+
+
+
+
+
